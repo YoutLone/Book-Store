79bc19722d710f5a059996298d4ed98d2bc96b07
Yout <youtlone95@gmail.com>
2023-07-13 11:13:24 +0630

    changes Book.js and ShowBooks.js file from feedback

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,19 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Book = ({ title }) => (
+const Book = ({ title, author }) => (
   <div>
     <h3>{title}</h3>
+    <p>
+      By
+      {author}
+    </p>
     <Button name="Remove" />
   </div>
 );
 
 Book.propTypes = {
   title: PropTypes.string,
+  author: PropTypes.string,
 };
 
 Book.defaultProps = {
-  title: 'Book tile',
+  title: 'Book title',
+  author: 'Unknown Author',
 };
 
 export default Book;
--- src/components/ShowBooks.js
+++ src/components/ShowBooks.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Book from './Book';
 import Form from './Form';
 
+const books = [
+  { id: 1, title: 'Book 1', author: 'Author 1' },
+  { id: 2, title: 'Book 2', author: 'Author 2' },
+];
+
 const ShowBooks = () => (
   <div>
-    <Book title="Book 1" />
-    <Book title="Book 2" />
+    {books.map((book) => (
+      <Book key={book.id} title={book.title} author={book.author} />
+    ))}
     <Form />
   </div>
 );
