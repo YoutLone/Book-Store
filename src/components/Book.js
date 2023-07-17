abf32d7834610b41b088ae9aa9f9248f772a0aeb
Yout <youtlone95@gmail.com>
2023-07-17 14:32:05 +0630

    add redux reducer and action

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
 import Button from './Button';
 
-const Book = ({ title, author }) => (
-  <div>
-    <h3>{title}</h3>
-    <p>
-      By
-      {author}
-    </p>
-    <Button name="Remove" />
-  </div>
-);
+const Book = ({ title, author }) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = () => {
+    dispatch(removeBook(1)); // Assuming book ID is 1
+  };
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>
+        By
+        {author}
+      </p>
+      <Button name="Remove" onClick={handleRemoveBook} />
+    </div>
+  );
+};
 
 Book.propTypes = {
   title: PropTypes.string,
